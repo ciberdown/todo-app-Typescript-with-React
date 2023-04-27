599a4d45fd5e7f6710e61a788c38ec20562d8619
ciberdown <teymuri.amin@gmail.com>
2023-04-27 20:49:22 +0330

    clg cleared

--- src/services/ReminderListService.tsx
+++ src/services/ReminderListService.tsx
@@ -4,13 +4,10 @@ class ReminderListServices {
   http = axios.create({ baseURL: "https://jsonplaceholder.typicode.com" });
   async getReminders() {
     const response = await this.http.get<ReminderInterface[]>("/todos");
-    console.log(response)
     return response.data;
   }
-
   async removeReminders(id: number){
     const response = await this.http.delete('/todos/'+id);
-    console.log(response)
   }
 
   async addReminder(title: string, userId: number){
